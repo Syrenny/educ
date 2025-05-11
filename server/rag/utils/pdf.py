3779feac4e6a63b17e99a7bba115e3d276312ff4
Syrenny <syrennyilya@yandex.ru>
2025-05-11 20:05:37 +0700

    feat: preprocessing for pdf with broken encodings

--- server/rag/utils/pdf.py
+++ server/rag/utils/pdf.py
@@ -1,6 +1,16 @@
 import fitz
 
 
+def smart_fix(text: str) -> str:
+    _t = ""
+    for c in text:
+        try:
+            _t += c.encode("latin1").decode("cp1251")
+        except UnicodeEncodeError:
+            _t += c
+    return _t
+
+
 def read_pdf(file: bytes) -> str:
     """
     Extracts text from a PDF file and returns it as a single string.
@@ -9,4 +19,4 @@ def read_pdf(file: bytes) -> str:
     :return: Concatenated text from all pages
     """
     doc = fitz.open(stream=file, filetype="pdf")
-    return "\n".join(page.get_text("text") for page in doc)
+    return "\n".join(smart_fix(page.get_text("text")) for page in doc)
